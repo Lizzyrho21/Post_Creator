446eee4f388ee1f3e29e4cb1325f34871c7ed12a
Lizzy Wiley <Lizzyrho@gmail.com>
2021-12-18 15:29:59 -0600

    CRUD DONE

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import ReadAllPosts
+from .views import *
 
-urlpatterns = [path('', ReadAllPosts.as_view(), name="posts"),
-
-]
+urlpatterns = [
+    path('', ReadAllPosts.as_view(), name="posts"),
+    path('<int:pk>/', ReadOnePost.as_view(), name="one_post"),
+    path('create/', CreateAPost.as_view(), name="create"),
+    path('<int:pk>/update', UpdatePost.as_view(), name="update"),
+    path('<int:pk>/delete', DeletePost.as_view(), name="delete"),
+    ]
--- posts/views.py
+++ posts/views.py
@@ -11,4 +11,26 @@ class ReadAllPosts(generics.ListAPIView):
     serializer_class = PostSerializer # Pulling in our serializer to translate into JSON data
 
 
+# Read one post
+class ReadOnePost(generics.RetrieveAPIView):
+    queryset = Post.objects.all() # A QuerySet represents a collection of objects from your database
+    serializer_class = PostSerializer # Pulling in our serializer to translate into JSON data
+
+# Create a post
+class CreateAPost(generics.ListCreateAPIView):
+    queryset = Post.objects.all() # A QuerySet represents a collection of objects from your database
+    serializer_class = PostSerializer # Pulling in our serializer to translate into JSON data
+
+# Update a post
+class UpdatePost(generics.RetrieveUpdateAPIView):
+    queryset = Post.objects.all() # A QuerySet represents a collection of objects from your database
+    serializer_class = PostSerializer # Pulling in our serializer to translate into JSON data
+
+# Delete a post
+class DeletePost(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all() # A QuerySet represents a collection of objects from your database
+    serializer_class = PostSerializer # Pulling in our serializer to translate into JSON data
+
+
+
 
