7534c144db0e5a7bf0b240e12e0a2be626ba9595
Lizzy Wiley <Lizzyrho@gmail.com>
2021-12-18 16:00:42 -0600

    started react server

---
+++ client/post_form/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import FormData from './components/FormData';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>Forms with React!!</h1>
+      <FormData />
+    </div>
+  );
+}
+
+export default App;
